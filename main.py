ef6e90b5954e258b5bc51de1c53f187ada719a12
egoing <egoing@gmail.com>
2015-06-10 06:49:40 +0900

    파일 생성시간 표시

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 __author__ = 'egoing'
 import sys, os, zlib
+
+def sorted_ls(path):
+    mtime = lambda f: os.stat(os.path.join(path, f)).st_mtime
+    return list(sorted(os.listdir(path), key=mtime))
+
+
 #path = sys.argv[1]
 path = '.'
 p_objects = path+'/.git/objects'
-for p in os.listdir(p_objects):
+for p in sorted_ls(p_objects):
     if p in ['info', 'pack']:
         continue
     for p2 in os.listdir(p_objects+'/'+p):
         fname = p_objects+'/'+p+'/'+p2
         with open(fname, 'rb') as f:
             data = f.read()
-        print(zlib.decompress(data))
+        print(p+p2+"\t"+str(os.stat(fname).st_mtime)+"\t"+str(zlib.decompress(data)))
+
 
