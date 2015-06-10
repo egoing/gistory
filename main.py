e48d8bd034d8916ff270476ceb38a73f58ae843d
egoing <egoing@gmail.com>
2015-06-10 21:02:53 +0900

    객체지향 스타일로 리팩토링

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
 __author__ = 'egoing'
 import sys, os, zlib
-
-def sorted_ls(path):
-    mtime = lambda f: os.stat(os.path.join(path, f)).st_mtime
-    return list(sorted(os.listdir(path), key=mtime))
-
+from abc import ABCMeta, abstractmethod
 
 #path = sys.argv[1]
+
+class Data(metaclass=ABCMeta):
+    filepath = None
+    _info = None
+    def __init__(self, filepath):
+        self.filepath = filepath;
+        self.parse()
+    def __str__(self):
+        return self._info['type']+'\t'+self._info['data']
+    @abstractmethod
+    def parse(self):
+        pass
+    @abstractmethod
+    def info(self):
+        pass
+
+class BlobData(Data):
+    def parse(self):
+        fileinfo = os.stat(self.filepath);
+        compressed_content = open(self.filepath, 'rb').read()
+        self._info = {
+            'type' : 'BLOB',
+            'name' : self.filepath,
+            'data' : str(zlib.decompress(compressed_content)),
+            'mtime' : fileinfo.st_mtime
+        }
+    def info(self):
+        return self._info
+
+class RefData(Data):
+    def parse(self):
+        fileinfo = os.stat(self.filepath);
+        content = open(self.filepath, 'rb').read()
+        self._info = {
+            'type' : 'REFE',
+            'name' : self.filepath,
+            'data' : str(content),
+            'mtime' : fileinfo.st_mtime
+        }
+    def info(self):
+        return self._info
 
 dlist = []
 
@@ -16,36 +53,20 @@ for p in os.listdir(p_objects):
     if p in ['info', 'pack']:
         continue
     for p2 in os.listdir(p_objects+'/'+p):
-        fname = p_objects+'/'+p+'/'+p2
-        with open(fname, 'rb') as f:
-            data = f.read()
-        dlist.append({
-            'type' : 'object',
-            'name' : p+p2,
-            'data' : str(zlib.decompress(data)),
-            'mtime' : os.stat(fname).st_mtime
-        })
-
-dlist.append({
-    'type': 'HEAD',
-    'name' : 'HEAD',
-    'data' : open('.git/HEAD').readline(),
-    'mtime' : os.stat('.git/HEAD').st_mtime
-})
+        dlist.append(BlobData(p_objects+'/'+p+'/'+p2))
 
 p_refs = path+'/.git/refs'
 for p in os.listdir(p_refs):
     for p2 in os.listdir(p_refs+'/'+p):
-        fname = p_refs+'/'+p+'/'+p2
-        with open(fname, 'rb') as f:
-            data = f.read()
-        dlist.append({
-            'type' : p,
-            'name' : p2,
-            'data' : open(fname).readline(),
-            'mtime' : os.stat(fname).st_mtime
-        })
+        dlist.append(RefData(p_refs+'/'+p+'/'+p2))
 
-dlist.sort(key=lambda x : x['mtime'], reverse=True)
-for p in dlist:
-    print(p['type'] +'\t'+ p['data'])
+dlist.append(RefData('.git/HEAD'))
+
+
+def call_sort(x):
+    info = x.info()
+    return info['mtime']
+dlist.sort(key=call_sort, reverse=True)
+
+for o in dlist:
+    print(o)
