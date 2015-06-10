e0ab0a63a9b29cad402f662b047aebb7bd654f67
egoing <egoing@gmail.com>
2015-06-10 11:43:09 +0900

    주요한 파일의 변화를 시간의 순서대로 출력하도록 변경

--- main.py
+++ main.py
@@ -7,15 +7,45 @@ def sorted_ls(path):
 
 
 #path = sys.argv[1]
+
+dlist = []
+
 path = '.'
 p_objects = path+'/.git/objects'
-for p in sorted_ls(p_objects):
+for p in os.listdir(p_objects):
     if p in ['info', 'pack']:
         continue
     for p2 in os.listdir(p_objects+'/'+p):
         fname = p_objects+'/'+p+'/'+p2
         with open(fname, 'rb') as f:
             data = f.read()
-        print(p+p2+"\t"+str(os.stat(fname).st_mtime)+"\t"+str(zlib.decompress(data)))
+        dlist.append({
+            'type' : 'object',
+            'name' : p+p2,
+            'data' : str(zlib.decompress(data)),
+            'mtime' : os.stat(fname).st_mtime
+        })
 
+dlist.append({
+    'type': 'HEAD',
+    'name' : 'HEAD',
+    'data' : open('.git/HEAD').readline(),
+    'mtime' : os.stat('.git/HEAD').st_mtime
+})
 
+p_refs = path+'/.git/refs'
+for p in os.listdir(p_refs):
+    for p2 in os.listdir(p_refs+'/'+p):
+        fname = p_refs+'/'+p+'/'+p2
+        with open(fname, 'rb') as f:
+            data = f.read()
+        dlist.append({
+            'type' : p,
+            'name' : p2,
+            'data' : open(fname).readline(),
+            'mtime' : os.stat(fname).st_mtime
+        })
+
+dlist.sort(key=lambda x : x['mtime'], reverse=True)
+for p in dlist:
+    print(p['type'] +'\t'+ p['data'])
