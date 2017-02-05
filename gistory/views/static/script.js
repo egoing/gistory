41708a4ca6b511e21ce76122128dff7106dbefd9
egoing <egoing@gmail.com>
2017-02-05 23:19:51 +0900

    항목을 선택했을 때 viewer가 최상단으로 이동하도록 개선

--- gistory/views/static/script.js
+++ gistory/views/static/script.js
@@ -5,8 +5,7 @@ function escapeHtml(text) {
         '>': '&gt;',
         '"': '&quot;',
         "'": '&#039;'
-    };;;;;
-
+    };
     return text.replace(/[&<>"']/g, function (m) {
         return map[m];
     })
@@ -29,28 +28,29 @@ function hashStringToColor(str) {
     return "#" + ("0" + r.toString(16)).substr(-2) + ("0" + g.toString(16)).substr(-2) + ("0" + b.toString(16)).substr(-2);
 }
 
+function scrollViewerToTop() {
+    if ($(document).width() > 992)
+        $('.viewer').offset({top: Math.max($('.element-list').offset().top, $(document).scrollTop() + 10)});
+}
+function setActive(target) {
+    $('a').removeClass('active');
+    target.addClass('active');
+}
 $('.element').on('click', function (e) {
     e.preventDefault();
     var _self = $(this);
-    $('html, body').animate({
-        scrollTop: $(this).offset().top - 10
-    }, 500);
+    setActive(_self);
     $.ajax({
         url: '/ajax/element',
         method: 'POST',
         success: function (result) {
-//        $('#viewer_table .type').html(result.type);
-//        $('#viewer_table .name').html(result.name);
-//        reg = /\b([a-f0-9]{40})\b/g;
-//        $('#viewer_table .data').html(nl2br(result.data.replace(reg, '<a href="#" class="sha1">$1</a>')));
             $('.viewer').html('');
             panelManager.add(0, result.type, result.name, result.data, result.path);
-            if ($(document).width() > 992)
-                $('.viewer').offset({top: $(_self).offset().top});
+            scrollViewerToTop();
         },
         data: {path: $(this).data('path')}
     })
-});;;;;
+});
 
 $(document).on('click', '.sha1', function (e) {
     e.preventDefault();
@@ -60,12 +60,12 @@ $(document).on('click', '.sha1', function (e) {
         url: '/ajax/object',
         method: 'POST',
         success: function (result) {
-            _parent.nextAll().remove();;;;;
+            _parent.nextAll().remove();
             panelManager.add(_parent.data('panel_id'), result.type, result.name, result.data)
         },
         data: {object: _object}
     })
-});;;;;
+});
 function Panel() {
     this.panel_id = 0;
     this.panels = [];
@@ -83,7 +83,7 @@ Panel.nl2br = function (str, is_xhtml) {
     var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br ' + '/>' : '<br>'; // Adjust comment to avoid issue on phpjs.org display
     return (str + '')
         .replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
-};;;;;
+};
 Panel.prototype.template = function (id, type, name, data, path) {
     var help = this.helps[type] ? '<span class="help"><a href="' + this.helps[type] + '" data-lity><img src="/static/movie.png"></a></span>' : '';
     var tag = $('<div class="panel panel-default" data-panel_id="' + id + '">\
@@ -93,9 +93,9 @@ Panel.prototype.template = function (id, type, name, data, path) {
                         <td class="data"><pre><code>' + data + '</code></pre></td>\
                     </tr>\
                 </table>\
-            </div>');;;;;
+            </div>');
     return tag;
-};;;;;
+};
 Panel.prototype.add = function (caller_panel_id, type, name, data, path) {
     reg = /\b([a-f0-9]{40})\b/g;
     data = escapeHtml(data);
@@ -103,13 +103,10 @@ Panel.prototype.add = function (caller_panel_id, type, name, data, path) {
     var new_panel = this.template(caller_panel_id + 1, type, name, data, path);
     this.panels.push(new_panel);
     $('.viewer').append(new_panel)
-};;;;;
-panelManager = new Panel();;;;;
+};
+panelManager = new Panel();
 
 $('.list-group a').each(function () {
     var $this = $(this);
     var path = $this.data('path');
-    // var color = hashStringToColor(path);
-    //$this.css('background-color', color);
-    // $this.css('border-left', "20px solid "+color);
-});;;;;
+});
