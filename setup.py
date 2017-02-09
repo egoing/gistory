4531ecae6a8804a644a1a7d0b2e0bdef642d9a63
egoing <egoing@gmail.com>
2017-02-09 10:46:33 +0900

    패키지 데이터를 하위 디렉토리까지 포함

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
+import os
 from codecs import open
 from os import path
 
@@ -12,9 +13,19 @@ from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+
+def get_long_description():
+    with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+        return f.read()
+
+
+def package_files(directory):
+    paths = []
+    for (_path, _directories, _filenames) in os.walk(directory):
+        for filename in _filenames:
+            paths.append(os.path.join('..', _path, filename))
+    return paths
+
 
 setup(
     name='gistory',
@@ -22,10 +33,10 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.37',
+    version='0.39',
 
     description='Tracking your .git changed history',
-    long_description=long_description,
+    long_description=get_long_description(),
 
     # The project's main homepage.
     url='https://github.com/egoing/git_changed_history',
@@ -90,7 +101,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'gistory' : ['views/main.tpl', 'views/static/*']
+        'gistory': package_files('gistory/views')
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
